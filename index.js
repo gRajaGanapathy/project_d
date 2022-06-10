2b90664f636930622a1d82db67fcca64c3963b4a
muralir <muralidharan@twilightsoftwares.com>
2022-06-10 13:06:55 +0530

    feta:changes files

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser= require('body-parser')
+const bodyParser = require("body-parser");
 const app = express();
 const config = require("./config/config");
-const user = require('./routers/user.routes')
+const fs = require("fs");
+// const user = require('./routers/user.routes')
 
 var corOptions = {
   origin: "http://localhost:3000",
@@ -14,13 +15,20 @@ app.use(cors(corOptions));
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 // app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
-app.use(bodyParser.text({ type: 'text/plain' }));
+app.use(bodyParser.text({ type: "text/plain" }));
 
-app.use("/", user);
+// app.use("/", user);
 //test
 
-app.get("/", (req, res) => {
- res.status(200).send("ok")
+app.post("/", (req, res) => {
+  const payload = JSON.stringify(req.body);
+
+  var file = "documents/sample.txt" + new Date();
+  fs.writeFileSync(file, payload, "binary", (err, result) => {
+    if (err) {
+      res.status(400).send(err);
+    } else [res.status(200).send("ok")];
+  });
 });
 
 const server = app.listen(config.PORT, () => {
